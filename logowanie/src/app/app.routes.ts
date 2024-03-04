e914b676bd871b447b76c4c06e4daf1736a90c1a
Proostoo <thexara123@gmail.com>
2024-03-04 17:47:01 +0100

    Added Logowanie component

---
+++ logowanie/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { LogowanieComponent } from './logowanie/logowanie.component';
+
+export const routes: Routes = [
+    { path: 'logowanie', component: LogowanieComponent }
+];
